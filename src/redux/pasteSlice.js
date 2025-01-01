d8740b74679bd625bcc91497828e673e7668ce77
abozdar821 <alimohsin821.am@gmail.com>
2024-12-31 21:28:24 -0500

    Update task_loggerSlice.js

--- src/redux/pasteSlice.js
+++ src/redux/pasteSlice.js
@@ -2,69 +2,69 @@ import { createSlice } from "@reduxjs/toolkit"
 import { toast } from "react-hot-toast"
 
 const initialState = {
-  pastes: localStorage.getItem("pastes")
-    ? JSON.parse(localStorage.getItem("pastes"))
+  task_logger: localStorage.getItem("task_logger")
+    ? JSON.parse(localStorage.getItem("task_logger"))
     : []
 }
 
-const pasteSlice = createSlice({
-  name: "paste",
+const task_loggerSlice = createSlice({
+  name: "task_logger",
   initialState,
   reducers: {
-    addToPastes: (state, action) => {
-      const paste = action.payload
-      const index = state.pastes.findIndex((item) => item._id === paste._id)
+    addToTask_Logger: (state, action) => {
+      const task_logger = action.payload
+      const index = state.task_logger.findIndex((item) => item._id === task_logger._id)
 
       if (index >= 0) {
         // If the course is already in the Pastes, do not modify the quantity
-        toast.error("Paste already exist")
+        toast.error("Task Logger already exists")
         return
       }
       // If the course is not in the Pastes, add it to the Pastes
-      state.pastes.push(paste)
+      state.task_logger.push(task_logger)
       
       // Update to localstorage
-      localStorage.setItem("pastes", JSON.stringify(state.pastes))
+      localStorage.setItem("task_logger", JSON.stringify(state.task_logger))
       // show toast
-      toast.success("Paste added")
+      toast.success("Task Logger added")
     },
 
-    updatePastes: (state, action) => {
-      const paste = action.payload
-      const index = state.pastes.findIndex((item) => item._id === paste._id)
+    updateTask_Logger: (state, action) => {
+      const task_logger = action.payload
+      const index = state.task_logger.findIndex((item) => item._id === task_logger._id)
 
       if (index >= 0) {
         // If the course is found in the Pastes, update it
-        state.pastes[index] = paste
+        state.task_logger[index] = task_logger
         // Update to localstorage
-        localStorage.setItem("pastes", JSON.stringify(state.pastes))
+        localStorage.setItem("task_logger", JSON.stringify(state.task_logger))
         // show toast
-        toast.success("Paste updated")
+        toast.success("Task Logger updated")
       }
     },
-    removeFromPastes: (state, action) => {
-      const pasteId = action.payload
+    removeFromTask_Logger: (state, action) => {
+      const task_loggerId = action.payload
 
-      console.log(pasteId)
-      const index = state.pastes.findIndex((item) => item._id === pasteId)
+      console.log(task_loggerId)
+      const index = state.task_logger.findIndex((item) => item._id === task_loggerId)
 
       if (index >= 0) {
         // If the course is found in the Pastes, remove it
-        state.pastes.splice(index, 1)
+        state.task_logger.splice(index, 1)
         // Update to localstorage
-        localStorage.setItem("pastes", JSON.stringify(state.pastes))
+        localStorage.setItem("task_logger", JSON.stringify(state.task_logger))
         // show toast
-        toast.success("Paste deleted")
+        toast.success("Task Logger deleted")
       }
     },
-    resetPaste: (state) => {
-      state.pastes = []
+    resetTask_Logger: (state) => {
+      state.task_logger = []
       // Update to localstorage
-      localStorage.removeItem("pastes")
+      localStorage.removeItem("task_logger")
     },
   },
 })
 
-export const { addToPastes, removeFromPastes, updatePastes } = pasteSlice.actions
+export const { addToTask_Logger, removeFromTask_Logger, updateTask_Logger } = pasteSlice.actions
 
-export default pasteSlice.reducer
+export default task_loggeerSlice.reducer
